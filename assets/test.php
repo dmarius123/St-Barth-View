5c44105afe2029e6c8b82ce307d380c1ad260877
Marius-Cristian Donea <me@mariuscristiandonea.com>
2011-05-26 17:47:54 +0300

    test

--- assets/test.php
+++ assets/test.php
@@ -1,9 +1,5 @@
 <?php
 
-    echo 'Test2';
-		echo "uite aici un alt test!";
-		echo "aici e un spatiu!";
-		echo "uite aici un alt test!";
-		echo "aici e un spatiu!";
-		echo "uite aici un alt test!";
+    echo "un test";
+
 ?>
