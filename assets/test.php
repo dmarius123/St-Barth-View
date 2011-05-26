8f7ceb1fce505b804fce20c7a636c98ad02d3508
andreicalistru <andro13ro@yahoo.com>
2011-05-26 18:02:15 +0300

    t

--- assets/test.php
+++ assets/test.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
     echo 'Test2';
 		echo "uite aici un alt test!";
 		echo "aici e un spatiu!";
@@ -7,4 +8,8 @@
 		echo "aici e un spatiu!";
 		echo "aici e un spatiu!";
 		echo "uite aici un alt test!";
+=======
+    echo "un test";
+
+>>>>>>> origin/master
 ?>
