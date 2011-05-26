3541f34d13b277e91912461d2698257e19610873
andreicalistru <andro13ro@yahoo.com>
2011-05-26 18:50:24 +0300

    t

--- assets/test.php
+++ assets/test.php
@@ -1,12 +1,12 @@
 <?php
 
-    echo "aici";
-    echo "am";
-    echo "modificat";
-    echo "eu am modificat aici";
-    echo "eu";
-		echo "si am mai pus o linie in plus";
-		echo "ultima modificare";
+    echo "test";
+    echo "";
+    echo "";
+    echo "";
+    echo "";
+		echo "";
+		echo "";
 
     echo "";
     echo "";
