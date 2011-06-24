568f99abbbcb3dcba5c2261b121d3753c5a16e86
andreicalistru <andro13ro@yahoo.com>
2011-06-24 23:31:22 +0300

    header dropdown menus

--- application/views/frontend/user/header/no-sign-in-navigation.php
+++ application/views/frontend/user/header/no-sign-in-navigation.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Title                   : St Barth View
  * File                    : application/views/frontend/user/header/no-sign-in-navigation.php
@@ -8,24 +7,52 @@
  * Created / Last Modified : 27 May 2011
  * Last Modified By        : Marius-Cristian Donea
  * Description             : Not loged in User header.
-*/
-
+ */
 ?>
-    <ul id="header-navigation">
-        <li><?=anchor('user', $header_sign_in)?></li>
-        <li class="separator">&nbsp;</li>
-        <li><?=anchor('user/sign-up/', $header_sign_up)?></li>
-        <li class="separator">&nbsp;</li>
-        <li><fb:login-button onlogin="facebookSignIn()" size="small" perms="email, user_about_me"><?=$header_sign_in?></fb:login-button></li>
-        <li class="clear" />
-    </ul>
-    <div id="fb-root"></div>
-    <script type="text/JavaScript">
-        window.fbAsyncInit = function(){
-            FB.init({appId:'148099351923386', status:true, cookie:true, xfbml:true});
-            FB.logout(function(response){
-                //alert(response.status)
-            });
-        };
-    </script>
-    <script type="text/JavaScript" src="http://connect.facebook.net/<?=$facebook_language?>/all.js"></script>
+<ul id="header-navigation">
+    <li class="language_menu" id="language_menu">
+        <div class="language_currency">
+            <div class="language_currency_display rounded_top">
+                <div class="language_currency_display_language flag sprite-en">&nbsp;</div>
+                <div class="language_currency_display_currency">$ USD</div>
+            </div>
+            <div class="language_currency_selector_container rounded" >
+                <ul class="language_currency_selector">
+                    <li class="instruction">Choose language...</li>
+                    <li class="language option" id="language_selector_en" name="en">
+                        <div class="flag sprite-en">&nbsp;</div>
+                        <div style="display:inline;padding-left:24px;">English</div>
+                    </li>
+                    <li class="language option" id="language_selector_fr" name="fr">
+                        <div class="flag sprite-fr">&nbsp;</div>
+                        <div style="display:inline;padding-left:24px;">Français</div>
+                    </li>
+                    <li class="dash">&nbsp;</li>
+                    <li class="instruction">Choose currency...</li>
+                    <li class="currency option" id="currency_selector_USD" name="USD"> <span>$</span> USD </li>
+                    <li class="currency option" id="currency_selector_EUR" name="EUR"> <span>€</span> EUR </li>
+                    <li class="language option reward_points_option">
+                        <div class="flag reward_points">&nbsp;</div>
+                        <div style="display:inline;padding-left:24px;">Rewards Points</div>
+                    </li>
+                </ul><!-- LANGUAGE_CURRENCY_SELECTOR -->
+            </div><!-- LANGUAGE_CURRENCY_SELECTOR_CONTAINER -->
+        </div><!-- LANGUAGE_CURRENCY -->
+    </li>
+    <li><?= anchor('user', $header_sign_in) ?></li>
+    <li class="separator">&nbsp;</li>
+    <li><?= anchor('user/sign-up/', $header_sign_up) ?></li>
+    <li class="separator">&nbsp;</li>
+    <li><fb:login-button onlogin="facebookSignIn()" size="small" perms="email, user_about_me"><?= $header_sign_in ?></fb:login-button></li>
+<li class="clear" />
+</ul>
+<div id="fb-root"></div>
+<script type="text/JavaScript">
+    window.fbAsyncInit = function(){
+        FB.init({appId:'148099351923386', status:true, cookie:true, xfbml:true});
+        FB.logout(function(response){
+            //alert(response.status)
+        });
+    };
+</script>
+<script type="text/JavaScript" src="http://connect.facebook.net/<?= $facebook_language ?>/all.js"></script>
--- application/views/frontend/user/header/sign-in-navigation.php
+++ application/views/frontend/user/header/sign-in-navigation.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Title                   : St Barth View
  * File                    : application/views/frontend/user/header/sign-in-navigation.php
@@ -8,19 +7,78 @@
  * Created / Last Modified : 27 May 2011
  * Last Modified By        : Marius-Cristian Donea
  * Description             : Loged in User header.
-*/
-
+ */
 ?>
-    <ul id="header-navigation">
-        <li><?=$header_welcome?>, <?=$first_name?> <?=$last_name?></li>
-        <li class="separator">&nbsp;</li>
-        <li><?=anchor('user/sign-out/', $header_sign_out)?></li>
-        <li class="clear" />
-    </ul>
-    <script type="text/JavaScript" src="http://connect.facebook.net/<?=$facebook_language?>/all.js"></script>
-    <script type="text/JavaScript">
-        window.fbAsyncInit = function(){
-            FB.init({appId:'148099351923386', status:true, cookie:true, xfbml:true});
-        };
-        //FB.logout();
-    </script>
+<ul id="header-navigation">
+    <li class="language_menu" id="language_menu">
+        <div class="language_currency">
+            <div class="language_currency_display rounded_top">
+                <div class="language_currency_display_language flag sprite-en">&nbsp;</div>
+                <div class="language_currency_display_currency">$ USD</div>
+            </div>
+            <div class="language_currency_selector_container rounded" >
+                <ul class="language_currency_selector">
+                    <li class="instruction">Choose language...</li>
+                    <li class="language option" id="language_selector_en" name="en">
+                        <div class="flag sprite-en">&nbsp;</div>
+                        <div style="display:inline;padding-left:24px;">English</div>
+                    </li>
+                    <li class="language option" id="language_selector_fr" name="fr">
+                        <div class="flag sprite-fr">&nbsp;</div>
+                        <div style="display:inline;padding-left:24px;">Français</div>
+                    </li>
+                    <li class="dash">&nbsp;</li>
+                    <li class="instruction">Choose currency...</li>
+                    <li class="currency option" id="currency_selector_USD" name="USD"> <span>$</span> USD </li>
+                    <li class="currency option" id="currency_selector_EUR" name="EUR"> <span>€</span> EUR </li>
+                    <li class="language option reward_points_option">
+                        <div class="flag reward_points">&nbsp;</div>
+                        <div style="display:inline;padding-left:24px;">Rewards Points</div>
+                    </li>
+                </ul><!-- LANGUAGE_CURRENCY_SELECTOR -->
+            </div><!-- LANGUAGE_CURRENCY_SELECTOR_CONTAINER -->
+        </div><!-- LANGUAGE_CURRENCY -->
+    </li>
+    <li class="separator">&nbsp;</li>
+    <li class="language_menu" id="profile_menu">
+        <div class="language_currency">
+            <div class="language_currency_display rounded_top">
+                <div class="language_currency_display_currency"><?= $first_name ?> <?= $last_name ?></div>
+            </div>
+            <div class="language_currency_selector_container rounded" >
+                <ul class="language_currency_selector">
+<!--                    <li class="instruction">My stuff</li>-->
+                    <li class="currency option"> Dashboard asd asd ds </li>
+                    <li class="currency option"> Offers </li>
+                    <li class="currency option"> Deals </li>
+                    <li class="currency option"> Messages </li>
+                    <li class="currency option"> Friends </li>
+                    <li class="currency option"> Reservations </li>
+                    <li class="currency option"> Comments </li>
+                    <li class="currency option"> Reward Points </li>
+                    <li class="currency option"> Settings </li>
+<!--                    <li class="dash">&nbsp;</li>
+                    <li class="instruction">Choose currency...</li>
+                    <li class="currency option" id="currency_selector_USD" name="USD"> <span>$</span> USD </li>
+                    <li class="currency option" id="currency_selector_EUR" name="EUR"> <span>€</span> EUR </li>
+                    <li class="language option reward_points_option">
+                        <div class="flag reward_points">&nbsp;</div>
+                        <div style="display:inline;padding-left:24px;">Rewards Points</div>
+                    </li>-->
+                </ul><!-- LANGUAGE_CURRENCY_SELECTOR -->
+            </div><!-- LANGUAGE_CURRENCY_SELECTOR_CONTAINER -->
+        </div><!-- LANGUAGE_CURRENCY -->
+
+    </li>
+<!--    <li><?= $header_welcome ?>, <?= $first_name ?> <?= $last_name ?></li>-->
+    <li class="separator">&nbsp;</li>
+    <li><?= anchor('user/sign-out/', $header_sign_out) ?></li>
+    <li class="clear" />
+</ul>
+<script type="text/JavaScript" src="http://connect.facebook.net/<?= $facebook_language ?>/all.js"></script>
+<script type="text/JavaScript">
+    window.fbAsyncInit = function(){
+        FB.init({appId:'148099351923386', status:true, cookie:true, xfbml:true});
+    };
+    //FB.logout();
+</script>
