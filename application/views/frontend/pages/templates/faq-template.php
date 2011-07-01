0e60e47407b51c754900d535f50b643799921c2e
andreicalistru <andro13ro@yahoo.com>
2011-07-01 10:43:43 +0300

    faq template options template

--- application/views/frontend/pages/templates/faq-template.php
+++ application/views/frontend/pages/templates/faq-template.php
@@ -14,14 +14,20 @@
 <?php $this->load->view('frontend/header'); ?>
     <div id="section" class="page-section">
         <div id="main" class="page-main">
-            <h1>This is H1 Test</h1>
-            <p>this is paragraph test this is paragraph test this is paragraph test this is paragraph test this is paragraph test this is paragraph test this is paragraph test this is paragraph test</p>
+            <h1>F.A.Q.</h1>
+            <h2>Qu’est-ce que St Barth View ?</h2>
+            <p>St Barh view s’impose comme la référence de la commercialisation promotionnelle de service. Une plateforme incontournable sur laquelle il vous sera proposé chaque jour une offre exceptionnelle dans les meilleurs établissements de votre ville. Restaurants branchés, brasseries traditionnelles, bars lounge, centres de remise en forme, cours de yoga, cours de cuisine et bien d’autres encore. Nos équipes recherchent au quotidien des offres qui surfent sur les tendances actuelles, tout en négociant des tarifs promotionnels exclusifs.</p>
             <p>here is a <a href="">test link</a> to see how it looks like</p>
+            <h2>Quelles sont les restrictions ?</h2>
+            <h2>Le deal d'aujourd'hui m'intéresse, comment puis-je en bénéficier ?</h2>
+            <h2>Mon deal vient d'être validé, que se passe-t-il à présent ?</h2>
         </div>
         <div id="sidebar" class="page-sidebar">
-            <h2>This is H2 Test</h2>
-            <p>this is paragraph test this is para graph test this is para graph test this is paragr aph test this is paragra ph test this is para graph test this is parag raph test this is para graph test</p>
+<!--            <h2>This is H2 Test</h2>-->
+<!--            <p>this is paragraph test this is para graph test this is para graph test this is paragr aph test this is paragra ph test this is para graph test this is parag raph test this is para graph test</p>-->
+        <?php $this->load->view('frontend/modules/templates/new-users-module-template'); ?>
         </div>
+
         <br class="clear">
     </div>
 <?php $this->load->view('frontend/footer'); ?>
