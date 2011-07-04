d093eb85b06560c7b036c50e1da68884ff3e765d
andreicalistru <andro13ro@yahoo.com>
2011-07-04 17:53:33 +0300

    messages

--- application/views/frontend/user/templates/messages-template.php
+++ application/views/frontend/user/templates/messages-template.php
@@ -3,13 +3,58 @@
 /*
  * Title                   : St Barth View
  * File                    : application/views/frontend/user/templates/messages-template.php
- * File Version            : 1.1
+ * File Version            : 1.2
  * Author                  : Marius-Cristian Donea
- * Created / Last Modified : 28 May 2011
- * Last Modified By        : Marius-Cristian Donea
+ * Created / Last Modified : 04 July 2011
+ * Last Modified By        : Andrei Calistru
  * Description             : Login User - Messages template.
 */
 
 ?>
+<!--    <h2><?=$user_messages_title?></h2>
+    <span class="title-separator"></span>-->
     <h2><?=$user_messages_title?></h2>
     <span class="title-separator"></span>
+<div class="messages-template">
+    <div class="messages-actions">
+        <span class="left"><a class="mess-actions" href="#">send new message</a></span>
+        <span class="right"><a class="mess-actions"href=""> &#9664;&nbsp;&nbsp;&nbsp;Messages</a> <a class="mess-actions" href="#"> Actions   &#9660; </a></span>
+        <br class="clear" />
+    </div>
+    <div class="messages-list">
+        <table>
+            <tr class="messages-item">
+                <td class="image">
+                    <a href="">
+                        <img height="60px" width="60px" alt="" src="https://graph.facebook.com/700676305/picture?type=large">
+                    </a>
+                </td>
+                <td class="content">
+                    <div class="message-author">Alexandre Abela</div>
+                    <div class="message-content">
+                        ala bala portocala ala bala portocala ala bala portocala ala bala portocala ala bala portocala ala bala portocala
+                    </div>
+                </td>
+                <td class="posted">
+                    5 minutes ago
+                </td>
+            </tr>
+            <tr class="messages-item">
+                <td class="image">
+                    <a href="">
+                        <img height="60px" width="60px" alt="" src="https://graph.facebook.com/700676305/picture?type=large">
+                    </a>
+                </td>
+                <td class="content">
+                    <div class="message-author">Alexandre Abela</div>
+                    <div class="message-content">
+                        ala bala portocala ala bala portocala ala bala portocala ala bala portocala ala bala portocala ala bala portocala
+                    </div>
+                </td>
+                <td class="posted">
+                    2 weeks
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
